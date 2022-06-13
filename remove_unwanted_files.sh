f2f01fc815d083ef6b8f4597a5c14195ee6b7b27
Michael Hashizume <michael.hashizume@puppet.com>
2022-06-13 10:41:56 -0700

    (maint) Updates cleanup shell script
    Ran Shellcheck on the script and fixed issues SC2035 and SC2045

--- remove_unwanted_files.sh
+++ remove_unwanted_files.sh
@@ -5,8 +5,22 @@ export PATH=/bin:/usr/bin
 grep -l '^# distrust=.' ./*.crt | xargs rm
 
 # Remove files that don't have serverAuth in the openssl-trust field
-for cert in $(ls *.crt); do
+for cert in ./*.crt
+do
+  [[ -e "$cert" ]] || break
   if ! grep -q 'serverAuth' "$cert"; then
     rm "$cert"
   fi
 done
+
+# Remove temporary files
+for tmp in ./*.tmp-p11-kit
+do
+  [[ -e "$tmp" ]] || break
+  rm "$tmp"
+done
+
+# Remove CA bundle
+if [[ -e ./ca-bundle.crt ]]; then
+  rm ./ca-bundle.crt
+fi
